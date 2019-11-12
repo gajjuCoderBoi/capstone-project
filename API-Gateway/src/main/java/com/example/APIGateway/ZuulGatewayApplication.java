46e1f7ff99fba1052c0222f027701c7e9bc37101
carloskruger <carloskr@yahoo.com>
2019-11-12 09:23:32 -0500

    API-Gateway set up

---
+++ API-Gateway/src/main/java/com/example/APIGateway/ZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
