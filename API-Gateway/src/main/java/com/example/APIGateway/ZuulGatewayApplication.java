3cc64ac3405aa0399785c83bf18ef3ccb3c52743
Mohammad Javed <ghazanfar9131@gmail.com>
2019-11-14 11:30:38 -0500

    - Fixed API-Gateway Issue.

--- API-Gateway/src/main/java/com/example/APIGateway/ZuulGatewayApplication.java
+++ API-Gateway/src/main/java/com/example/APIGateway/ZuulGatewayApplication.java
@@ -1,9 +1,13 @@
-package com.example.APIGateway;
+package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
