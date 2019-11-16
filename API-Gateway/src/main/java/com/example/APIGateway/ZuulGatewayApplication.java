760a27227eba300c461db27e67108b2b2e41950e
Mohammad Javed <ghazanfar9131@gmail.com>
2019-11-16 14:07:40 -0500

    Deployment in progress...

--- API-Gateway/src/main/java/com/example/APIGateway/ZuulGatewayApplication.java
+++ API-Gateway/src/main/java/com/example/APIGateway/ZuulGatewayApplication.java
@@ -4,14 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
 	}
+
+
+
+
 
 }
---
+++ deployment.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Building eureka-server
+cd eureka-server/
+mvn package
+docker build -t ghazanfar9131/eureka-server:0.0.1-SNAPSHOT .
+cd ..
+
+# Building api-gateway
+cd api-gateway/
+mvn package
+docker build -t ghazanfar9131/api-gateway:0.0.1-SNAPSHOT .
+cd ..
+
+# Building users-api
+cd users-api/
+mvn package
+docker build -t ghazanfar9131/users-api:0.0.1-SNAPSHOT .
+cd ..
+
+# Building profile-api
+cd profile-api/
+mvn package
+docker build -t ghazanfar9131/profile-api:0.0.1-SNAPSHOT .
+cd ..
+
+# Building posts-api
+cd posts-api
+mvn package
+docker build -t ghazanfar9131/posts-api:0.0.1-SNAPSHOT .
+cd ..
+
+# Building comments-api
+cd comments-api
+mvn package
+docker build -t ghazanfar9131/comments-api:0.0.1-SNAPSHOT .
+cd ..
+
+# Pusing Docker-images
+docker push ghazanfar9131/eureka-server:0.0.1-SNAPSHOT
+docker push ghazanfar9131/api-gateway:0.0.1-SNAPSHOT
+docker push ghazanfar9131/users-api:0.0.1-SNAPSHOT
+docker push ghazanfar9131/profile-api:0.0.1-SNAPSHOT
+docker push ghazanfar9131/posts-api:0.0.1-SNAPSHOT
+docker push ghazanfar9131/comments-api:0.0.1-SNAPSHOT
+
+
+# CF Pushing
+# Building eureka-server
+cd eureka-server/
+#cf delete ghazanfar9131-eureka-server
+cf push
+cd ..
+
+# Building api-gateway
+cd api-gateway/
+#cf delete ghazanfar9131-api-gateway
+cf push
+cd ..
+
+# Building users-api
+cd users-api/
+#cf delete ghazanfar9131-users-api
+cf push
+cd ..
+
+# # Building profile-api
+# cd profile-api/
+# cf delete ghazanfar9131-profile-api
+# cf push
+# cd ..
+
+# # Building posts-api
+# cd posts-api
+# cf delete ghazanfar9131-posts-api
+# cf push
+# cd ..
+
+# # Building comments-api
+# cd comments-api
+# cf delete ghazanfar9131-comments-api
+# cf push
+# cd ..
