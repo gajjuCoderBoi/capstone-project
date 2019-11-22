6d4cb077f211b80a1e0b8b1fa2d725caf11f266f
carloskruger <carloskr@yahoo.com>
2019-11-22 16:50:46 -0500

    Comments error exception

---
+++ comments-api/src/main/java/com/ga/commentsapi/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ga.commentsapi.exception;
+
+public class EntityNotFoundException extends Exception {
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
