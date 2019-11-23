3e7d69d53ea4305f0a35f3b319d1e7a984950a50
Mohammad Javed <ghazanfar9131@gmail.com>
2019-11-23 12:41:30 -0500

    - Setting up Swagger.

--- swagger-app/src/main/java/com/ga/swaggerapp/SwaggerAppApplication.java
+++ swagger-app/src/main/java/com/ga/swaggerapp/SwaggerAppApplication.java
@@ -2,9 +2,15 @@ package com.ga.swaggerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
+@EnableScheduling
+@EnableEurekaClient
 public class SwaggerAppApplication {
 
 	public static void main(String[] args) {
