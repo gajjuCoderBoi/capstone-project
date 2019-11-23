80cf75f3db45f2416b77b12cd973f56e933331dc
Mohammad Javed <ghazanfar9131@gmail.com>
2019-11-22 23:16:20 -0500

    - Created Swagger Centralized Spring Boot Application.

---
+++ swagger-app/src/main/java/com/ga/swaggerapp/SwaggerAppApplication.java
@@ -0,0 +1,14 @@
+package com.ga.swaggerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@SpringBootApplication
+public class SwaggerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerAppApplication.class, args);
+	}
+
+}
---
+++ swagger-app/src/test/java/com/ga/swaggerapp/SwaggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.swaggerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
