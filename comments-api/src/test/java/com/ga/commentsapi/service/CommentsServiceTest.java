5ec4ac93b1918707ef3b9f3bd4fbc820c05a6b77
carloskruger <carloskr@yahoo.com>
2019-11-28 11:57:33 -0500

    post test

---
+++ comments-api/src/test/java/com/ga/commentsapi/service/CommentsServiceTest.java
@@ -0,0 +1,124 @@
+package com.ga.commentsapi.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ga.commentsapi.bean.Post;
+import com.ga.commentsapi.bean.User;
+import com.ga.commentsapi.exception.CommentNotExistException;
+import com.ga.commentsapi.exception.PostNotExistException;
+import com.ga.commentsapi.exception.TokenException;
+import com.ga.commentsapi.exception.UnauthorizeActionException;
+import com.ga.commentsapi.messagequeue.Sender;
+import com.ga.commentsapi.model.Comment;
+import com.ga.commentsapi.repository.CommentRepository;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class CommentsServiceTest {
+
+    private static final String USERNAME = "user";
+    private static final String TOKEN = "abcdef";
+
+
+    @InjectMocks
+    private CommentServiceImpl commentService;
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @InjectMocks
+    private Comment comment;
+
+    @InjectMocks
+    private User user;
+
+    @InjectMocks
+    private Post post;
+
+
+
+    @InjectMocks
+    private ObjectMapper objectMapper;
+
+
+    @Mock
+    Sender sender;
+
+    @Before
+    public void initDummies() {
+        user.setUsername(USERNAME);
+        user.setUserId(1L);
+        post.setTitle("Example post title");
+        post.setText("Example post description is cool.");
+        post.setPostId(1L);
+        post.setUserId(1L);
+        comment.setUser(user);
+        comment.setPostId(1L);
+        comment.setUserId(1L);
+        comment.setId(1L);
+        comment.setText("This is a simnple comment");
+    }
+
+    @Test
+    public void createComment() throws TokenException, PostNotExistException {
+
+        when(sender.getUserFromUserAPI(any())).thenReturn(user);
+        when(sender.getPostByPostId(any())).thenReturn(post);
+        when(commentRepository.save(comment)).thenReturn(comment);
+        Comment actual = commentService.createComment(comment,1L, TOKEN);
+
+        assertEquals(comment, actual);
+
+    }
+
+    @Test
+    public void deleteCommentByCommentId() throws TokenException, UnauthorizeActionException, PostNotExistException {
+
+        when(commentRepository.findById(any())).thenReturn(java.util.Optional.of(comment));
+        Comment savedComment = commentRepository.findById(comment.getId()).orElse(null);
+
+        if (savedComment.getId().longValue() != user.getUserId().longValue())
+            throw new UnauthorizeActionException("Unauthorized Action.");
+        Comment actual = savedComment;
+        commentRepository.delete(savedComment);
+        assertEquals(comment, actual);
+    }
+/*
+    @Test
+    public void getCommentsByUser(User user) {
+
+        commentRepository.save(comment);
+        Long userid = when(user.getUserId();
+        commentRepository.findCommentsbyUserId(userid);
+
+        /*
+        Long userid = when(user.getUserId();
+        commentRepository.findCommentsbyUserId(userid);
+         */
+
+   // }
+
+
+
+
+}
--- posts-api/src/test/java/com/ga/postsapi/service/PostServiceTest.java
+++ posts-api/src/test/java/com/ga/postsapi/service/PostServiceTest.java
@@ -1,19 +1,34 @@
 package com.ga.postsapi.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ga.postsapi.bean.Comment;
 import com.ga.postsapi.bean.User;
+import com.ga.postsapi.exception.PostNotExistException;
 import com.ga.postsapi.exception.TokenException;
+import com.ga.postsapi.exception.UnauthorizeActionException;
 import com.ga.postsapi.messagequeue.Sender;
 import com.ga.postsapi.model.Post;
 import com.ga.postsapi.repository.PostRepository;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -21,6 +36,9 @@ public class PostServiceTest {
 
     private static final String USERNAME = "user";
     private static final String TOKEN = "abcdef";
+
+//    @Spy
+//    private PostServiceImpl postService;
 
     @InjectMocks
     private PostServiceImpl postService;
@@ -33,6 +51,10 @@ public class PostServiceTest {
 
     @InjectMocks
     private User user;
+
+
+    @InjectMocks
+    private ObjectMapper objectMapper;
 
 
     @Mock
@@ -58,4 +80,51 @@ public class PostServiceTest {
         assertEquals(post, actual);
 
     }
+
+    @Test
+    public void deletePost() throws TokenException, UnauthorizeActionException, PostNotExistException {
+
+        when(postRepository.findById(any())).thenReturn(java.util.Optional.of(post));
+        Post savedPost = postRepository.findById(post.getPostId()).orElse(null);
+        if (savedPost == null) throw new PostNotExistException("Post Doesn't Exist.");
+        if (savedPost.getUserId().longValue() != user.getUserId().longValue())
+            throw new UnauthorizeActionException("Unauthorized Action.");
+        sender.deleteCommentsOfPost(savedPost.getPostId());
+        Post actual = savedPost;
+        postRepository.delete(savedPost);
+        assertEquals(post, actual);
+    }
+
+    @Test
+    public void postList() {
+        Post post1 = new Post();
+        List<Post> addedPosts = new ArrayList<Post>();
+        post1.setTitle("Example post1 title");
+        post1.setText("Example post1 description is cool.");
+        post1.setPostId(2L);
+        post1.setUser(user);
+        post1.setUserId(1L);
+        Post post2 = new Post();
+        post2.setTitle("Example post2 title");
+        post2.setText("Example post2 description is cool.");
+        post2.setPostId(3L);
+        post2.setUser(user);
+        post2.setUserId(1L);
+        Post post3 = new Post();
+        post3.setTitle("Example post3 title");
+        post3.setText("Example post3 description is cool.");
+        post3.setPostId(4L);
+        post3.setUser(user);
+        post3.setUserId(1L);
+        addedPosts.add(post1);
+        addedPosts.add(post2);
+        addedPosts.add(post3);
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
+        List<Post> actualPosts = addedPosts;
+
+        assertEquals(addedPosts, actualPosts);
+    }
+
 }
