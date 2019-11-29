7d11be10d37285cc9714ac17a323e0ce97c2142e
Mohammad Javed <ghazanfar9131@gmail.com>
2019-11-28 19:22:09 -0500

    - fixing tests

--- posts-api/src/test/java/com/ga/postsapi/service/PostServiceTest.java
+++ posts-api/src/test/java/com/ga/postsapi/service/PostServiceTest.java
@@ -49,7 +49,6 @@ public class PostServiceTest {
 
     @Before
     public void initDummies() {
-        MockitoAnnotations.initMocks(this);
         user.setUsername(USERNAME);
         user.setUserId(1L);
         post.setTitle("Example post title");
